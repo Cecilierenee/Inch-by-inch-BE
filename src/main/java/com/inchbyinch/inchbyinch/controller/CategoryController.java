a259ffdab021f7297b3dc4af27724675d981d18f
Cecilie Robb <Cecilie_robb@mymail.eku.edu>
2022-01-27 16:21:33 -0500

    refactoring category controller

--- src/main/java/com/inchbyinch/inchbyinch/controller/CategoryController.java
+++ src/main/java/com/inchbyinch/inchbyinch/controller/CategoryController.java
@@ -1,9 +1,8 @@
 package com.inchbyinch.inchbyinch.controller;
 
-import com.inchbyinch.inchbyinch.exceptions.InformationNotFoundException;
-import com.inchbyinch.inchbyinch.exceptions.InformationExistException;
 import com.inchbyinch.inchbyinch.model.Category;
-import com.inchbyinch.inchbyinch.repository.CategoryRepository;
+
+import com.inchbyinch.inchbyinch.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,71 +13,40 @@ import java.util.Optional;
 @RequestMapping(path = "/api")
 public class CategoryController {
 
-    private CategoryRepository categoryRepository;
+    private CategoryService categoryService;
 
     @Autowired
-    public void setCategoryRepository(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
+    public void setCategoryService(CategoryService categoryService) {
+        this.categoryService = categoryService;
     }
 
     @GetMapping(path = "/categories/")
     public List<Category> getCategories() {
-        return categoryRepository.findAll();
+        return categoryService.getCategories();
     }
 
     @GetMapping(path = "/categories/{categoryId}")
     public Optional getCategory(@PathVariable Long categoryId) {
-        System.out.println( "getting the category with the id of " + categoryId);
-        Optional category = categoryRepository.findById(categoryId);
-        if (category.isPresent()) {
-            return category;
-        } else {
-            throw new InformationNotFoundException("Category with id " + categoryId + " not found");
-        }
+        System.out.println("getting the category with the id of " + categoryId);
+        return categoryService.getCategory(categoryId);
     }
 
     @PostMapping("/categories/")
     public Category createCategory(@RequestBody Category categoryObject) {
         System.out.println("creating a category ");
-        Category category = categoryRepository.findByName(categoryObject.getName());
-        if (category != null) {
-            throw new InformationExistException("category with name " + category.getName() + " already exist");
-        } else {
-            return categoryRepository.save(categoryObject);
-        }
+        return categoryService.createCategory(categoryObject);
     }
 
     @PutMapping("/categories/{categoryId}")
     public Category updateCategory(@PathVariable(value = "categoryId") Long categoryId, @RequestBody Category categoryObject) {
-        System.out.println("updating the category")
-        Optional<Category> category = categoryRepository.findById(categoryId);
-        if (category.isPresent()) {
-            if (categoryObject.getName().equals(category.get().getName())) {
-                System.out.println("Same name");
-                throw new InformationExistException("Category already exist");
-            } else {
-                Category updateCategory = categoryRepository.findById(categoryId).get();
-                updateCategory.setName(categoryObject.getName());
-                updateCategory.setDescription(categoryObject.getDescription());
-                return categoryRepository.save(updateCategory);
-            }
-        } else {
-            throw new InformationNotFoundException("Category not found");
-        }
+        System.out.println("updating the category");
+        return categoryService.updateCategory(categoryId, categoryObject);
     }
 
     @DeleteMapping("/categories/{categoryId}")
     public Optional<Category> deleteCategory(@PathVariable(value = "categoryId") Long categoryId) {
         System.out.println("deleting the category with the id of " + categoryId);
-        Optional<Category> category = categoryRepository.findById(categoryId);
-
-        if(category.isPresent()) {
-            categoryRepository.deleteById(categoryId);
-            return category;
-        } else {
-            throw new InformationNotFoundException("Category not found");
-        }
-
+        return categoryService.deleteCategory(categoryId);
     }
 }
 
