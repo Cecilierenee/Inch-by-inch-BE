548d784e3d5c04f7465ab1567da8d76063a56607
Cecilie Robb <Cecilie_robb@mymail.eku.edu>
2022-01-27 21:29:38 -0500

    add routine repository

---
+++ src/main/java/com/inchbyinch/inchbyinch/repository/RoutineRepository.java
@@ -0,0 +1,16 @@
+package com.inchbyinch.inchbyinch.repository;
+
+import com.inchbyinch.inchbyinch.model.Routine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RoutineRepository extends JpaRepository<Routine, Long> {
+    Routine findByName(String routineName);
+    
+    Routine findByNameAndIdIsNot(String routineName, Long routineId);
+
+    List<Routine> findByCategoryId(Long routineId);
+}
