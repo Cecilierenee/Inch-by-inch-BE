efe4b9496f9b1248550f76c83f6d22a688b535ce
Cecilie Robb <Cecilie_robb@mymail.eku.edu>
2022-01-28 01:22:16 -0500

    adding userprofile model

--- src/main/java/com/inchbyinch/inchbyinch/model/User.java
+++ src/main/java/com/inchbyinch/inchbyinch/model/User.java
@@ -101,4 +101,9 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", userName='" + userName + '\'' + ", emailAddress='" + emailAddress + '\'' + ", password='" + password + '\'' + '}';
+    }
 }
