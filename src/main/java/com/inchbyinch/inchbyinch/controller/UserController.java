0ab5684fca42437a176f69f1bdade8aeff14b7e8
Cecilie Robb <Cecilie_robb@mymail.eku.edu>
2022-01-28 02:16:55 -0500

    adding User controller

---
+++ src/main/java/com/inchbyinch/inchbyinch/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.inchbyinch.inchbyinch.controller;
+
+import com.inchbyinch.inchbyinch.model.User;
+import com.inchbyinch.inchbyinch.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/auth/users")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User createUser(@RequestBody User userObject) {
+        System.out.println("calling createUser");
+        return userService.createUser(userObject);
+    }
+
+}
